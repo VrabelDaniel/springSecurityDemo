d373b003111c68abf983d9cb8467603bc066d2d8
VrabelD <vrabel.daniel@kesmarki.com>
2024-09-30 15:35:04 +0200

    create demo project for spring security

---
+++ src/main/java/com/example/demo/model/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+}
